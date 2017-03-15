544bc324977b45208ce468572bce687d839e840f
David Holloway <dhollowa@google.com>
2017-03-15 12:22:52 -0700

    Add test scaffold that runs.

---
+++ spec/pipe_utils-spec.js
@@ -0,0 +1,11 @@
+'use babel';
+
+import pipeUtils from '../lib/pipe_utils';
+
+describe('PipeUtils', () => {
+  describe('when something happens', () => {
+    it('compares apples to apples', () => {
+      expect("apples").toEqual("apples")
+    })
+  })
+})
